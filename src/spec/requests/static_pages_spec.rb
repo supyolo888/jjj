a3e5bda12dfae7c2922c97d6a4fc59172394923f
supyolo888 <supyolo888@gmail.com>
2021-10-27 01:49:10 +0900

    title

--- src/spec/requests/static_pages_spec.rb
+++ src/spec/requests/static_pages_spec.rb
@@ -8,7 +8,7 @@ RSpec.describe "StaticPages", type: :request do
     it "returns http success" do
       get "/"
       expect(response).to have_http_status(:success)
-      assert_select "title", "Home | #{base_title}"
+      assert_select "title", "#{base_title}"
     end
   end
 
@@ -16,7 +16,7 @@ RSpec.describe "StaticPages", type: :request do
     it "returns http success" do
       get "/static_pages/home"
       expect(response).to have_http_status(:success)
-      assert_select "title", "Home | #{base_title}"
+      assert_select "title", "#{base_title}"
     end
   end
 
