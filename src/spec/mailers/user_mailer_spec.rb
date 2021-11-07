ce0a08de23289e46d0f257ac0575dc250e18a394
supyolo888 <supyolo888@gmail.com>
2021-11-07 20:11:15 +0900

    Add password reset

--- src/spec/mailers/user_mailer_spec.rb
+++ src/spec/mailers/user_mailer_spec.rb
@@ -1,6 +1,7 @@
 require "rails_helper"
 
 RSpec.describe UserMailer, type: :mailer do
+
   describe "account_activation" do
 
     let(:user) { FactoryBot.create(:user) }
@@ -19,5 +20,26 @@ RSpec.describe UserMailer, type: :mailer do
     end
 
   end
-  
+
+  describe "password_reset" do
+    
+    before do
+      user.reset_token = User.new_token
+    end
+
+    let(:user) { FactoryBot.create(:user) }
+    let(:mail) { UserMailer.password_reset(user) }
+
+    it "renders the headers" do
+      expect(mail.subject).to eq("Password reset")
+      expect(mail.to).to eq([user.email])
+      expect(mail.from).to eq(["noreply@example.com"])
+    end
+    it "renders the body" do
+      expect(mail.body.encoded).to match(user.reset_token)
+      expect(mail.body.encoded).to match(CGI.escape(user.email))
+    end
+
+  end
+
 end
