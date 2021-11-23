a8b2d80e51ff96c367bc90b2397af08b2a70ce44
supyolo888 <supyolo888@gmail.com>
2021-11-23 22:53:17 +0900

    re profile

--- src/db/seeds.rb
+++ src/db/seeds.rb
@@ -1,37 +1,37 @@
-# ユーザー
-User.create!(name:  "Example User",
-  email: "example@railstutorial.org",
-  password:              "foobar",
-  password_confirmation: "foobar",
-  image_name: "test.JPG",
-  admin:     true,
-  activated: true,
-  activated_at: Time.zone.now)
+# # ユーザー
+# User.create!(name:  "Example User",
+#   email: "example@railstutorial.org",
+#   password:              "foobar",
+#   password_confirmation: "foobar",
+#   image_name: "default_user.jpeg",
+#   admin:     true,
+#   activated: true,
+#   activated_at: Time.zone.now)
 
-99.times do |n|
-name  = Faker::Name.name
-email = "example-#{n+1}@railstutorial.org"
-password = "password"
-User.create!(name:  name,
-    email: email,
-    password:              password,
-    password_confirmation: password,
-    image_name: "default_user.jpeg",
-    activated: true,
-    activated_at: Time.zone.now)
-end
+# 99.times do |n|
+# name  = Faker::Name.name
+# email = "example-#{n+1}@railstutorial.org"
+# password = "password"
+# User.create!(name:  name,
+#     email: email,
+#     password:              password,
+#     password_confirmation: password,
+#     image_name: "default_user.jpeg",
+#     activated: true,
+#     activated_at: Time.zone.now)
+# end
 
-# マイクロポスト
-users = User.order(:created_at).take(6)
-50.times do
-content = Faker::Lorem.sentence(word_count: 5)
-users.each { |user| user.microposts.create!(content: content) }
-end
+# # マイクロポスト
+# users = User.order(:created_at).take(6)
+# 50.times do
+# content = Faker::Lorem.sentence(word_count: 5)
+# users.each { |user| user.microposts.create!(content: content) }
+# end
 
-# 以下のリレーションシップを作成する
-users = User.all
-user  = users.first
-following = users[2..50]
-followers = users[3..40]
-following.each { |followed| user.follow(followed) }
-followers.each { |follower| follower.follow(user) }
+# # 以下のリレーションシップを作成する
+# users = User.all
+# user  = users.first
+# following = users[2..50]
+# followers = users[3..40]
+# following.each { |followed| user.follow(followed) }
+# followers.each { |follower| follower.follow(user) }
