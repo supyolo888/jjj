b7d89f3466e87c5827ef03f1f4157c203c443f78
supyolo888 <supyolo888@gmail.com>
2021-11-24 05:10:02 +0900

    re

--- src/app/models/user.rb
+++ src/app/models/user.rb
@@ -19,7 +19,7 @@ class User < ApplicationRecord
   has_secure_password
   validates :password, presence: true, length: { minimum: 6 }, allow_nil: true
 
-  mount_uploader :image, ImageUploader
+  mount_uploader :avatar, AvatarUploader
 
   # 渡された文字列のハッシュ値を返す
   def User.digest(string)
