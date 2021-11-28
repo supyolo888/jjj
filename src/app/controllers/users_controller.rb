0e241b9c5794c2f4a5c1e71bdeeccc08d7b1809c
supyolo888 <supyolo888@gmail.com>
2021-11-28 21:48:01 +0900

    search

--- src/app/controllers/users_controller.rb
+++ src/app/controllers/users_controller.rb
@@ -5,7 +5,14 @@ class UsersController < ApplicationController
   before_action :admin_user,     only: :destroy
 
   def index
-    @users = User.paginate(page: params[:page])
+    if params[:q] && params[:q].reject { |key, value| value.blank? }.present?
+      @q = User.ransack(search_params, activated_true: true)
+      @title = "Search Result"
+    else
+      @q = User.ransack(activated_true: true)
+      @title = "All users"
+    end
+    @users = @q.result.paginate(page: params[:page])
   end
 
   def show
@@ -83,6 +90,10 @@ class UsersController < ApplicationController
 
     def admin_user
       redirect_to(root_url) unless current_user.admin?
+    end
+
+    def search_params
+      params.require(:q).permit(:name_cont)
     end
 
 end
