5c1dfa3900767ec87d8f3463c0f506a8544a00a3
supyolo888 <supyolo888@gmail.com>
2021-12-22 22:39:21 +0900

    node

--- src/app/javascript/packs/application.js
+++ src/app/javascript/packs/application.js
@@ -2,6 +2,6 @@ require("@rails/ujs").start()
 require("turbolinks").start()
 require("@rails/activestorage").start()
 require("channels")
-// require("jquery")
-// import "bootstrap"
-// import '@fortawesome/fontawesome-free/js/all';
+require("jquery")
+import "bootstrap"
+import '@fortawesome/fontawesome-free/js/all';
